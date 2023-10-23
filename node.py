64df6cbba15cde7c71d8c701d9e2d55317c6a797
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2023-10-23 10:48:29 +0100

    mudanças

--- node.py
+++ node.py
@@ -6,24 +6,57 @@ class FSNode:
         self.ip = ip
         self.port = port
         self.files = {}  # Dicionário de ficheiros que este nó possui
+        self.tracker_ip = None
+        self.tracker_port = None
+
+    def set_tracker_info(self, tracker_ip, tracker_port):
+        self.tracker_ip = tracker_ip
+        self.tracker_port = tracker_port
 
     def connect_to_tracker(self, tracker_ip, tracker_port):
-        # Simula a conexão com o rastreador (tracker)
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((tracker_ip, tracker_port))
-        registration_data = f"{self.ip},{self.port}"
+        file2 = "File1"
+        registration_data = f"REGISTER,{self.ip},{self.port},{file2}"
         client_socket.send(registration_data.encode('utf-8'))
         client_socket.close()
+        node.set_tracker_info(tracker_ip, tracker_port)
+
         print(f"Node at {self.ip}:{self.port} registered with the tracker")
 
     def download_file(self, filename):
-        # Simula o processo de download de um ficheiro
-        print(f"Node at {self.ip}:{self.port} is downloading {filename}")
+        if not self.tracker_ip or not self.tracker_port:
+            print("Erro: informações do rastreador em falta. Conecte-se ao rastreador primeiro.")
+            return
+
+        query_data = f"GET {filename}"
+        tracker_response = self.query_tracker(query_data)
+
+        if tracker_response.startswith("FILE_FOUND"):
+            _, node_ip_port = tracker_response.split(" ", 1)
+            node_ip, node_port = node_ip_port.split(":")
+            print(f"File '{filename}' is available at {node_ip}:{node_port}")
+            self.download_from_node(node_ip, int(node_port), filename)
+        else:
+            print(f"File '{filename}' not found in the network.")
+
+    def query_tracker(self, query_data):
+        # Simula a consulta ao rastreador usando uma conexão TCP
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((self.tracker_ip, self.tracker_port))
+        client_socket.send(query_data.encode('utf-8'))
+        response = client_socket.recv(1024).decode('utf-8')
+        client_socket.close()
+        return response
+
+    def download_from_node(self, node_ip, node_port, filename):
+        # Implementar download do ficheiro do nó específico por UDP
+        # Abrir uma nova conexão UDP e iniciar o download
+        print(f"Node at {self.ip}:{self.port} is downloading '{filename}' from {node_ip}:{node_port}")
 
 if __name__ == "__main__":
-    # Configurações do nó
     node_ip = "127.0.0.2"
-    node_port = 5001
+    node_port = 9091
 
     node = FSNode(node_ip, node_port)
 
@@ -33,6 +66,8 @@ if __name__ == "__main__":
 
     node.connect_to_tracker(tracker_ip, tracker_port)
 
-    # Mantém o nó em execução indefinidamente
+    # Exemplo de download de um ficheiro
+    node.download_file("File1")
+
     while True:
         pass
--- tracker.py
+++ tracker.py
@@ -6,10 +6,9 @@ class FSTracker:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
-        self.nodes = []  # Lista de nós registados no rastreador
+        self.nodes = {}  # Dicionário de nós (peers) e ficheiros que eles possuem
 
     def start(self):
-        # Inicia o servidor do rastreador
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((self.ip, self.port))
         server_socket.listen(5)
@@ -17,16 +16,40 @@ class FSTracker:
 
         while True:
             client_socket, client_address = server_socket.accept()
-            client_thread = threading.Thread(target=self.handle_node_registration, args=(client_socket,))
+            client_thread = threading.Thread(target=self.handle_node_message, args=(client_socket,))
             client_thread.start()
 
-    def handle_node_registration(self, client_socket):
-        # Lida com o registo de um nó no rastreador
-        data = client_socket.recv(1024)
-        node_info = data.decode('utf-8').split(',')
-        node_ip, node_port = node_info
-        self.nodes.append((node_ip, int(node_port)))
-        print(f"Node registered: {node_ip}:{node_port}")
+    def handle_node_message(self, client_socket):
+        data = client_socket.recv(1024).decode('utf-8')
+        print("TESTE 1 -> " + str(data))
+        if data.startswith("REGISTER"):
+            _, node_ip, node_port, files = data.split(',')
+            print("TESTE 2 -> " + str(node_ip))
+            print("TESTE 3 -> " + str(node_port))
+            print("TESTE 4 -> " + str(files))
+            files = files.split(';')  # Os nomes dos ficheiros são separados por ';'
+            self.nodes[(node_ip, int(node_port))] = set(files)
+            print(f"Node registered: {node_ip}:{node_port}")
+        else:
+            # Aqui, trata consultas de ficheiros
+            # Por exemplo, 'GET File1'
+            # Implemente a lógica para responder a consultas de ficheiros aqui
+            filename = data[4:]  # Remove "GET " para obter o nome do ficheiro
+            nodes_with_file = [(node, files) for node, files in self.nodes.items() if filename in files]
+            if nodes_with_file:
+                node_ip, node_port = nodes_with_file[0][0]
+                response = f"FILE_FOUND {node_ip}:{node_port}"
+                client_socket.send(response.encode('utf-8'))
+            else:
+                response = "FILE_NOT_FOUND"
+                client_socket.send(response.encode('utf-8'))
+
+    def query_file(self, filename):
+        # Simula a consulta do nó ao rastreador para obter informações sobre o ficheiro
+        nodes_with_file = [node for node, files in self.nodes.items() if filename in files]
+        if nodes_with_file:
+            return nodes_with_file[0]
+        return None
 
 if __name__ == "__main__":
     # Configurações do rastreador
